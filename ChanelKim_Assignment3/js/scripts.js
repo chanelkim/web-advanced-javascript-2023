f221ef73067fbad30bcf2735a58824a5851aede3
Chanel Kim (she/her) <65513296+chanelkim@users.noreply.github.com>
2023-02-16 10:04:12 -0500

    Add files via upload

---
+++ ChanelKim_Assignment3/js/scripts.js
@@ -0,0 +1,53 @@
+/* 
+---------- Flexible Function Assignment -------------- 
+Pattern Generator using multiple functions, callbacks and parameters that makes your pattern creating method as flexible as possible. Play around by tweaking the arguments so that by changing a few parameters, your function creates something unexpected and unique. Then put them together into a sequence of function calls.
+*/
+
+// GOAL - create letterform out of series of symbols and spaces, made of a 5x5 arrangement of one ASCII symbol
+
+//Step 1 - create function that produces a series of 0s and 1s (length 5)
+function arrGenerator() {
+  //generates random array of 0s and 1s
+  let arr = [];
+  for (let i = 0; i < 5; i++) {
+    arr.push(Math.round(Math.random()));
+  }
+  return arr;
+}
+const arr = arrGenerator(); //store array in variable
+console.log(arr);
+
+// *** NEED HELP HERE ***
+//Step 2 - create function that prints 0 as a space, 1 as the variable ASCII character from the randomly generated array
+let character = "º";
+function symbolGenerator(symbol, array) {
+  let space = "-";
+  for (let i = 0; i < array.length; i++) {
+    // for (const i in array) {
+    if (array[i] == 0) {
+      return space;
+    } else {
+      return symbol;
+    }
+  }
+  return symbolGenerator();
+}
+// }
+const symbols = symbolGenerator(character, arr); //store symbolized array in variable
+console.log(symbols);
+
+//Step 3 - create function that stacks the randomly generated arrays (5 lines tall)
+function stackGenerator() {
+  let stack = [];
+  for (let i = 0; i < 5; i++) {
+    stack.push(arrGenerator());
+  }
+  return stack;
+}
+const stack = stackGenerator(); //store in stack of arrays in variable
+console.log(stack);
+
+// *** NEED HELP HERE ***
+//Step 4 - loop through the array of arrays, symbolizing each array to create a 5x5 symbol stack, a pattern that hopefully emulates a letterform
+const pattern = symbolGenerator(character, stack); //stores symbolized stack in a variable
+console.log(pattern);
